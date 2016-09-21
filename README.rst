ae25e63a56818450b8da2ddf8a19c772416fa27a
Guillaume Piot <guillaume@cotidia.com>
2016-09-21 17:36:07 +0100

    Initial commit

---
+++ README.rst
@@ -0,0 +1,5 @@
+Cotidia Documentation Module
+============================
+
+Provide an admin and an API for internal and external documentation available
+as a single page or through the API.
---
+++ doc/models.py
@@ -0,0 +1,22 @@
+import uuid
+
+from django.db import models
+
+
+class Document(models.Model):
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False)
+
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+    body = models.TextField(blank=True)
+
+    is_private = models.BooleanField(default=True)
+
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('title',)
+
+    def __str__(self):
+        return self.title
